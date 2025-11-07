1b81079cfbea2b948081421fb7e6d123c24f46c4
sebastianhernandezm17-max <sebastianhernandezm17@gmail.com>
2025-11-07 07:49:03 -0500

    Add files via upload

---
+++ MayorDosNumerosEnteros.py
@@ -0,0 +1,8 @@
+num1  = float(input("Ingresa el primer numero: ")) 
+num2  = float(input("Ingresa el segundo numero: ")) 
+if num1> num2:
+  print("El primer numero es mayor al segundo")
+elif num1==num2:
+  print("El primero es igual al segundo")
+else:
+  print("El segundo numero es mayor al primero")
---
+++ NotaCalificacion17Oct.py
@@ -0,0 +1,9 @@
+x = float(input("Ingresa su nota: ")) 
+if x < 3.0:
+  print("Insuficiente")
+elif x <= 3.5:
+  print("Aceptable")
+elif x <= 4.0:
+  print("Sobresaliente")
+elif x <= 5.0:
+  print("Excelente")
