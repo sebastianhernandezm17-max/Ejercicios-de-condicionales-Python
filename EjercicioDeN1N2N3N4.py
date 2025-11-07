3d67ce4a4838f3dd4a79f829d3022726368c5250
sebastianhernandezm17-max <sebastianhernandezm17@gmail.com>
2025-11-07 07:28:15 -0500

    Add files via upload

---
+++ EjercicioDeN1N2N3N4.py
@@ -0,0 +1,13 @@
+n1 = float(input("Ingrese el valor a n1: "))
+n2 = float(input("Ingrese el valor a n2: "))
+n3 = float(input("Ingrese el valor a n3: "))
+n4 = float(input("Ingrese el valor a n4: "))
+if n1 > n2 and n1 > n3 and n1 > n4:
+  print("El mayor es n1, seria:",n1)
+elif n2 > n1 and n2 > n3 and n2 > n4:
+  print("El mayor es n2, seria:",n2)
+elif n3 > n1 and n3 > n2 and n3 > n4:
+  print("El mayor es n3, seria:",n3)
+elif n4 > n1 and n4 > n2 and n4 > n3:
+  print("El mayor es n4, seria:",n4)
+
---
+++ FelicitacionesCalificacion.py
@@ -0,0 +1,5 @@
+notaDefinitiva= float(input("Ingrese su nota definitiva: ")) 
+if notaDefinitiva >= 4.0:
+  print("Felicitaciones")
+else:
+  print("Reprobaste")
