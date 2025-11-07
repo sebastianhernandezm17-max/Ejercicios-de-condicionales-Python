c6e17ad83c33f0903fcef71ebaaa8b9faaca7372
sebastianhernandezm17-max <sebastianhernandezm17@gmail.com>
2025-11-07 08:06:38 -0500

    Add files via upload

---
+++ NumeroPrimoOOOOOOO.py
@@ -0,0 +1,5 @@
+num = float(input("Ingrese un numero del 0 al 20: ")) 
+if num == 2 or 3 or 5 or 7 or 11 or 13 or 17 or 19:
+  print(num, "Es un numero primo")
+elif num == 0 or 1 or 4 or 6 or 8 or 9 or 10 or 12 or 14 or 15 or 16 or 18 or 20:
+  print(num, "No es un numero primo")
